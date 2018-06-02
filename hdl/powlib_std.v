d6a35b08dca07d2051da8e3d5f82fe93114dc43c
Andrew Powell <andrewandrepowell2@gmail.com>
2018-06-02 14:05:12 -0500

    Added the flip flop synchronizer for simple one-directional clock domain crossings.

--- hdl/powlib_std.v
+++ hdl/powlib_std.v
@@ -1,13 +1,8 @@
 `timescale 1ns / 1ps
 
-module powlib_std(
-
-    );
-endmodule
-
 module powlib_flipflop(d,q,clk,rst,vld);
 
-  parameter integer         W    = 16;    // Width
+  parameter integer         W    = 1;    // Width
   parameter reg     [W-1:0] INIT = 0;    // Initial value
   parameter reg             EAR  = 0;    // Enable asynchronous reset
   parameter reg             EVLD = 0;    // Enable valid
@@ -37,4 +32,36 @@ module powlib_flipflop(d,q,clk,rst,vld);
     end  
   end
 
+endmodule
+
+module powlib_ffsync(d,q,aclk,bclk,arst,brst,vld);
+
+  parameter integer         W    = 16;    // Width
+  parameter reg     [W-1:0] INIT = 0;    // Initial value
+  parameter reg             EAR  = 0;    // Enable asynchronous reset
+  parameter reg             EVLD = 0;    // Enable valid
+  parameter integer         S    = 2;    // Number of B clk domain stages
+  input     wire    [W-1:0] d;           // Input data
+  input     wire            vld;         // Valid  
+  input     wire            aclk, bclk;  // Clock
+  input     wire            arst, brst;  // Reset
+  output    wire    [W-1:0] q;           // Output data  
+  
+            genvar          i;
+            wire    [W-1:0] ds [0:S];
+            wire    [W-1:0] aq;
+  
+  powlib_flipflop #(.W(W),.INIT(INIT),.EAR(EAR),.EVLD(EVLD))  aff (.d(d),.q(aq),.clk(aclk),.rst(arst),.vld(vld));
+  
+  for (i=0; i<S; i=i+1) begin
+    powlib_flipflop #(.W(W),.INIT(INIT),.EAR(EAR))  bffs (.d(ds[i]),.q(ds[i+1]),.clk(bclk),.rst(brst));
+  end
+  
+  if (S<1) begin
+    assign q     = aq;
+  end else begin
+    assign ds[0] = aq;
+    assign q     = ds[S];
+  end 
+  
 endmodule
