da2bab02d3d774032d37b25723b15e08290fa9c6
Andrew Powell <andrewandrepowell2@gmail.com>
2018-06-03 20:48:21 -0500

    Alright! Looking good. Now I can start to build up my simulation stuff. Feels good.

---
+++ tcf/test_flipflop/test_flipflop.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module test_flipflop(d,q,clk,rst,vld);
+       
+  parameter              W    = 1;    // Width
+  parameter      [W-1:0] INIT = 0;    // Initial value
+  parameter              EAR  = 0;    // Enable asynchronous reset
+  parameter              EVLD = 0;    // Enable valid  
+  input     wire [W-1:0] d;           // Input data
+  input     wire         vld;         // Valid  
+  input     wire         clk;         // Clock
+  input     wire         rst;         // Reset
+  output    wire [W-1:0] q;           // Output data    
+
+  powlib_flipflop #(.W(W),.INIT(INIT),.EAR(EAR),.EVLD(EVLD)) dut (.d(d),.q(q),.clk(clk),.rst(rst),.vld(vld));               
+    
+endmodule
