04bf3b28c3ea452d83ee31b254a1d4439780a4e2
Andrew Powell <andrewandrepowell2@gmail.com>
2018-06-06 01:21:35 -0500

    This python library structure sucks so much.

--- tcf/test_flipflop/test_flipflop.py
+++ tcf/test_flipflop/test_flipflop.py
@@ -1,22 +1,23 @@
 import random
 import logging
 
-import cocotb
+from cocotb import test, coroutine
 
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge, ReadOnly
-from cocotb.drivers import BitDriver
-from cocotb.drivers.avalon import AvalonSTPkts as AvalonSTDriver
-from cocotb.drivers.avalon import AvalonMaster
-from cocotb.monitors.avalon import AvalonSTPkts as AvalonSTMonitor
 from cocotb.regression import TestFactory
 from cocotb.scoreboard import Scoreboard
 from cocotb.result import TestFailure
 
+from powlib.drivers import FlipflopDriver
 
-@cocotb.test()
+import powlib
+
+@test()
 def test_flipflop(dut):
     """
     
     """
+
     yield Timer(50,"us")
+
